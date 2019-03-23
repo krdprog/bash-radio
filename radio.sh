94b2e4137f33cdd00b47d57cb62dcbe37a0547dd
krdprog <info@krdprog.ru>
2019-03-23 08:50:59 +0300

    Some fix

--- radio.sh
+++ radio.sh
@@ -45,7 +45,20 @@ case $1 in
   13)
     OpenRadio "MAXIMUM" "http://icecast.radiomaximum.cdnvideo.ru:8000/max_m.aac" ;
     ;;
+  14)
+    OpenRadio "Absolutley Country Hits" "http://strm112.1.fm/acountry_mobile_mp3" ;
+    ;;
+  15)
+    OpenRadio "Klik FM Cyprus" "http://eco.onestreaming.com:8310/stream" ;
+    ;;
+  16)
+    OpenRadio "Radio Hersonisos" "http://178.59.22.125:8081/live" ;
+    ;;
+  17)
+    OpenRadio "Birds" "http://sc-brazilianbirds.1.fm:10072/" ;
+    ;;
   add)
+
     echo "данный функционал пока не реализован" ;
     ;;
   play)
@@ -59,17 +72,21 @@ echo "
 === Список станций: ========================================
 + 1 - Record Remix : radio 1
 + 2 - Radio Record Breaks : radio 2
-+ 3 - Radio Record : radio 4
-+ 4 - Future House : radio 5
-+ 5 - EDM : radio 6
-+ 6 - Megamix : radio 7
-+ 7 - Gold : radio 8
-+ 8 - Trancemission : radio 9
-+ 9 - Pirate Station : radio 10
-+ 10 - Deep : radio 11
-+ 11 - Vip House : radio 12
-+ 12 - Neurofunk : radio 13
-+ 13 - MAXIMUM : radio 14
++ 3 - Radio Record : radio 3
++ 4 - Future House : radio 4
++ 5 - EDM : radio 5
++ 6 - Megamix : radio 6
++ 7 - Gold : radio 7
++ 8 - Trancemission : radio 8
++ 9 - Pirate Station : radio 9
++ 10 - Deep : radio 10
++ 11 - Vip House : radio 11
++ 12 - Neurofunk : radio 12
++ 13 - MAXIMUM : radio 13
++ 14 - Absolutley Country Hits : radio 14
++ 15 - Klik FM Cyprus : radio 15
++ 16 - Radio Hersonisos : radio 16
++ 17 - Birds : radio 17
 
 + play - играть по URL : radio play [url]
 + add - добавить новую станцию : radio add
